700468a3150862e3e6a629e531f25f4fde2b15c1
AlvaroCtm <86389654+AlvaroCtm@users.noreply.github.com>
2021-07-14 19:25:45 -0500

    Update trianguloreves.java

--- trianguloreves.java
+++ trianguloreves.java
@@ -3,7 +3,17 @@ package diagramas.de.fujo;
 public class trianguloreves {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner pr = new Scanner(System.in);
+		System.out.println("ingrese un numero numero");
+		int N = pr.nextInt();
+
+		for (int i = N; i >= 1; i--) {
+			System.out.println("");
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+		}
+		System.out.println("");
 
 	}
 
